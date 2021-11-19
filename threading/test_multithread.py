8cbab0b2c3b727c4c03ca0d2cf3da92cc5b66182
choig <Gerald 7273463+choig@users.noreply.github.com>
2021-11-19 07:10:06 -0500

    update threading example

--- threading/test_multithread.py
+++ threading/test_multithread.py
@@ -1,10 +1,10 @@
 """
 Sample for threading, asyncio, and loguru
 """
-# import time
-# import sys
+import sys
 import random
 import uuid
+import logging
 import threading
 import asyncio
 from loguru import logger
@@ -16,23 +16,24 @@ def trigger_event(tlog, prefix):
         tlog.debug(f"{prefix}: That's it, beautiful logging.")
         tlog.info(f"{prefix}: Here is an info tidbit --- at {count} times.")
         tlog.error(f"{prefix}: Whoa, an error is found here.")
-        # time.sleep(1)
 
 #------------------------------------------------------------------------------
 async def trigger_event_async(tlog, prefix):
-    """ Quick event for threading with logging """
+    """ Quick event for threading with logging with async """
     while True:
         msg_id = str(uuid.uuid4())
         tlog.debug(f"{prefix}: That's it, beautiful logging.")
         tlog.info(f"{prefix}: Here is an info tidbit -------  {msg_id}.")
         tlog.error(f"{prefix}: Whoa, an error is found here.")
+        # Test without f-string
+        tlog.error("%s: legacy %s" % (prefix, msg_id)) # pylint: disable=consider-using-f-string
         tlog.info(f"{prefix}: ===============================")
-        # time.sleep(10)
         await asyncio.sleep(random.random())
 
 #------------------------------------------------------------------------------
-def example_threading(tlog):
+def example_threading():
     """ Use standard threading module """
+    tlog = logging.getLogger(__name__)
     thr1 = threading.Thread(target=trigger_event, args=(tlog, 'thread1',) )
     thr2 = threading.Thread(target=trigger_event, args=(tlog, 'thread2',) )
 
@@ -44,15 +45,16 @@ def example_threading(tlog):
     tlog.info("Complete.")
 
 #------------------------------------------------------------------------------
-def example_asyncio(tlog):
+def example_asyncio():
     """ Sample of asyncio """
     # queue = asyncio.Queue()
     loop = asyncio.get_event_loop()
+    tlog = logger # relying on loguru logging
 
     tlog.info("Hi from asyncio")
     try:
-        loop.create_task(trigger_event_async(tlog,"async_thread_001"))
-        loop.create_task(trigger_event_async(tlog,"async_thread_002"))
+        loop.create_task(trigger_event_async(tlog, "async_thread_001"))
+        loop.create_task(trigger_event_async(tlog, "async_thread_002"))
         loop.run_forever()
     except KeyboardInterrupt:
         tlog.info("Processing interrupted")
@@ -60,17 +62,36 @@ def example_asyncio(tlog):
         loop.close()
         tlog.info("Successfully shutdown service.")
 
-
 #==============================================================================
 if __name__ == "__main__":
-    # logger.add(sys.stdout,
-    #            colorize=True,
-    #            format="{time:YYYY-MM-DD at HH::mm:ss} | {level} | {message}")
-    logger.add("logs/file.log",
-        rotation="100 KB", # rotation="1 MB",
-        # format="{time:YYYY-MM-DD at HH::mm:ss} | {level} | {message}",
-        compression="zip",
-        retention=2)
 
-    # example_threading(logger)
-    example_asyncio(logger)
+    #--------------------------------------------------------------------------
+    # """ LOGURU """
+    logger.remove() # remove default stderr for one stdout logger
+    logger.add(sys.stdout, colorize=True)
+    # logger.add("logs/file.log",
+    #     rotation="100 KB", # rotation="1 MB",
+    #     # format="{time:YYYY-MM-DD at HH::mm:ss} | {level} | {message}",
+    #     compression="zip",
+    #     retention=2)
+    #--------------------------------------------------------------------------
+    # """ Standard logging """
+    slogger = logging.getLogger(__name__)
+    slogger.setLevel(logging.DEBUG)
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler('logs/sfile.log')
+    # """ Create formatters and add it to handlers """
+    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+    # """ Add handlers to the logger """
+    slogger.addHandler(c_handler)
+    slogger.addHandler(f_handler)
+    #--------------------------------------------------------------------------
+
+    # Standard thread and logging
+    example_threading()
+
+    # Asyncio and loguru
+    example_asyncio()

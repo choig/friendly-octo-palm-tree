293a71df597a453a5367bfc6618dd5eae92311e4
choig <Gerald 7273463+choig@users.noreply.github.com>
2021-12-12 11:12:44 -0500

    more example test experiments

--- examplethread/tests/test_multithread.py
+++ examplethread/tests/test_multithread.py
@@ -2,19 +2,21 @@
 from .context import src
 
 import pytest
-import threading
 import os
+import glob
 import logging
+from loguru import logger
 import src.multithread as gb1
 
 class TestThread:
-    """ Test suite """
-
+    """ Test suite """  
     def setup(self):
         """ setup some modules """
-        print("DDD: {}" , os.getcwd())
-        if os.path.isfile('logs/testfile.log'):
-            os.remove('logs/testfile.log')
+        try:
+            for fhxp in glob.glob("logs/testfile*.log"):
+                os.unlink(fhxp)
+        except PermissionError as excp:
+            print("ERROR: removing file - {}", str(excp))
 
     def teardown(self):
         """ teardown once complete """
@@ -27,26 +29,52 @@ class TestThread:
         """ per method teardown """
         try:
             gb1.LOCK.release()
+            for fhxp in glob.glob("logs/testfile*.log"):
+                os.unlink(fhxp)
         except RuntimeError as excp:
             pass
+        except PermissionError as excp:
+            print("ERROR: removing file - {}", str(excp))
+
+    # ---------------------------------------------------------------------------------------------
+    loop_count = 0
+    def test_loop(self):
+        self.loop_count += 1
+        if self.loop_count > 2:
+            return False
+        return True
+    # ---------------------------------------------------------------------------------------------
 
     def test_run_increase(self):
-        # pytest.skip("skipping this test until handle global lock")
         gb1.increase("zzz", 5)
         assert gb1.ZCOUNT == 5
 
     def test_run_increase_with_negative_value(self):
-        # pytest.skip("skipping this test until handle global lock")
         gb1.increase("zzz", -10)
         assert gb1.ZCOUNT == -10
 
-    def test_trigger_event(self):
+    def test_trigger_event(self, request):
+        logname = "logs/testfile-" + request.node.name + ".log"
         tlog = logging.getLogger("test")
         tlog.setLevel(logging.INFO)
         c_handler = logging.StreamHandler()
-        f_handler = logging.FileHandler('logs/testfile.log')
+        f_handler = logging.FileHandler(logname)
         tlog.addHandler(f_handler)
         gb1.trigger_event(tlog,  "quicktest", duration=5)
-        with open(file="logs/testfile.log", mode="r") as fhxp:
+        with open(file=logname, mode="r") as fhxp:
             sl2list = fhxp.readlines()
+        fhxp.close()
         assert sl2list[len(sl2list) - 2] == "quicktest: Here is an info tidbit --- at 5 times.\n"
+
+    @pytest.mark.asyncio
+    async def test_trigger_event_async(self, request):
+        # pytest.skip("skipping on how to use async")
+        logname = "logs/testfile-" + request.node.name + ".log"
+        logger.remove() # remove default stderr for one stdout logger
+        logger.add(logname, colorize=True, level="INFO") # DEBUG is default
+        zlog = logger
+        await gb1.trigger_event_async(zlog, "zzz", self.test_loop)
+        with open(file=logname, mode="r") as fhxp:
+            sl2list = fhxp.readlines()
+        fhxp.close()
+        assert len(sl2list) == 6
